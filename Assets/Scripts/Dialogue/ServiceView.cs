32cc700b7063b207205ce4ea6d0611a1b15ac711
TomasRacciatti <tomasracciatti@gmail.com>
2025-09-07 15:38:46 -0300

    Servicee view

---
+++ Assets/Scripts/Dialogue/ServiceView.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+namespace Dialogue
+{
+    public abstract class ServiceView : MonoBehaviour
+    {
+        protected IDialogueService dialogueService;
+
+        protected virtual void OnEnable()
+        {
+            dialogueService = DialogueManager.Instance;
+            if (dialogueService != null)
+                Subscribe(true);
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (dialogueService != null)
+                Subscribe(false);
+            dialogueService = null;
+        }
+
+        protected abstract void Subscribe(bool on);
+    }
+}
