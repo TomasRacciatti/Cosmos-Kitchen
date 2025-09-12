7f3f993ee9702a2f227c35f8f794472bf77b0432
TomasRacciatti <tomasracciatti@gmail.com>
2025-09-12 11:29:25 -0300

    Hookup de variables

--- Assets/Scripts/Book/BookHandler.cs
+++ Assets/Scripts/Book/BookHandler.cs
@@ -5,6 +5,8 @@ namespace Book
     public class BookHandler : MonoBehaviour
     {
         [SerializeField] private Animator animator;
+        [SerializeField] private AudioClip openSfx;
+        [SerializeField] private AudioClip closeSfx;
         private bool _bookOpen = false;
         
         public bool IsOpen => _bookOpen;
@@ -25,6 +27,7 @@ namespace Book
             animator.speed = 1;
             animator.Play(_bookOpen ? "OpenBook" : "CloseBook", 0, 
                 1 - Mathf.Clamp01(animator.GetCurrentAnimatorStateInfo(0).normalizedTime));
+            AudioManager.instance.PlaySFX(_bookOpen ? openSfx : closeSfx);
             return _bookOpen;
         }
     }
