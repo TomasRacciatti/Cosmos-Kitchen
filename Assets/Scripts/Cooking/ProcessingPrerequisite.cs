53b4fb1ec96bf9e1de94955baa7d5c8f32b71960
TomasRacciatti <tomasracciatti@gmail.com>
2025-11-05 12:22:54 -0300

    ProcessingPrerequisite.cs

---
+++ Assets/Scripts/Cooking/ProcessingPrerequisite.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace Cooking
+{
+    [Serializable]
+    public class ProcessingPrerequisite : MonoBehaviour
+    {
+        [Tooltip("Cooking method that requires prerequisite")]
+        public CookingMethod methodToPerform;
+    
+        [Tooltip("Method that must be completed before this method can be executed")]
+        public CookingMethod prerequisiteMethod;
+    }
+}
